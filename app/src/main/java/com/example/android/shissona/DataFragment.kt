0441de6855794949488a3a2d7903a9e79d290f05
arepade.obiri <aobiri@digicoreltd.com>
2023-03-20 19:49:49 +0100

    Change list in DataFragment.kt to Jetpack Compose's lazyColumn

--- app/src/main/java/com/example/android/shissona/DataFragment.kt
+++ app/src/main/java/com/example/android/shissona/DataFragment.kt
@@ -6,11 +6,32 @@ import android.app.AlertDialog
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import com.example.android.shissona.Util.Companion.CHART_COLORS
+import com.example.android.shissona.Util.Companion.ITEMS
 import com.example.android.shissona.database.Expense
 import com.example.android.shissona.databinding.FragmentDataBinding
 import com.example.android.shissona.viewModels.DataViewModel
@@ -18,13 +39,14 @@ import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
+import kotlinx.coroutines.flow.collectLatest
 import java.text.NumberFormat
 
 
 class DataFragment : Fragment() {
     private var _binding: FragmentDataBinding? = null
     private val binding get() = _binding!!
-    private var triggerComposeEmail:(()->Unit)?=null
+    private var triggerComposeEmail: (() -> Unit)? = null
 
     private val viewModel: DataViewModel by viewModels()
 
@@ -50,6 +72,80 @@ class DataFragment : Fragment() {
 
         _binding = FragmentDataBinding.bind(view)
         setupObservers()
+        setupViews()
+    }
+
+    private fun setupViews() {
+        binding.composeView.setContent {
+            ListView()
+        }
+    }
+
+    @Composable
+    private fun ListView() {
+        val list by viewModel.getAllExpensesForTheMonth().collectAsState(initial = emptyList())
+
+
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            itemsIndexed(list) { _, item ->
+                MaterialTheme() {
+                    Column(modifier = Modifier.fillMaxWidth().clickable {
+                        AlertDialog
+                            .Builder(this@DataFragment.requireContext())
+                            .setMessage(
+                                "Description: ${item.description}\n Cost: ${
+                                    NumberFormat
+                                        .getCurrencyInstance()
+                                        .format(
+                                            item.expensePrice
+                                        )
+                                }"
+                            )
+                            .setPositiveButton("Done") { _, _ -> composeAndSendEmail(list)}
+                            .create()
+                            .show()
+                    }) {
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Image(
+                                painter = painterResource(id = ITEMS[item.expenseType].imageResource),
+                                contentDescription = ITEMS[item.expenseType].name,
+                                modifier = Modifier
+                                    .height(24.dp)
+                                    .width(24.dp),
+                                colorFilter = ColorFilter.tint(colorResource(id = ITEMS[item.expenseType].colorTint))
+                            )
+
+                            Text(
+                                text = Util.getFullDateAndTime(item.entryTime),
+                                modifier = Modifier.weight(1f),
+                                textAlign = TextAlign.Center,
+                                maxLines = 1
+                            )
+
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_keyboard_arrow_right_black_24dp),
+                                contentDescription = "",
+                                colorFilter = ColorFilter.tint(colorResource(id = android.R.color.darker_gray))
+                            )
+
+                        }
+
+                        Divider(
+                            color = colorResource(id = android.R.color.darker_gray),
+                            modifier = Modifier.alpha(0.4f)
+                        )
+                    }
+                }
+
+            }
+        }
+
     }
 
 
@@ -109,64 +205,52 @@ class DataFragment : Fragment() {
 
 
     private fun setupObservers() {
-        viewModel.getAllExpensesForTheMonth().observe(viewLifecycleOwner, Observer { list ->
-                val pieEntries = ArrayList<PieEntry>()
-                chartList(list).forEach {
-                    pieEntries.add(PieEntry(it.price, Util.ITEMS[it.expenseType].name))
+        lifecycleScope.launchWhenResumed {
+            viewModel.getAllExpensesForTheMonth().collectLatest { list ->
+                try {
+                    val pieEntries = ArrayList<PieEntry>()
+                    chartList(list).forEach {
+                        pieEntries.add(PieEntry(it.price, ITEMS[it.expenseType].name))
 
-                }
+                    }
 
-                triggerComposeEmail = {
-                    composeAndSendEmail(list)
-                }
+                    triggerComposeEmail={
+                        composeAndSendEmail(list)
+                    }
 
-                val dataSet = PieDataSet(pieEntries, "Total Expenses")
-                dataSet.setColors(CHART_COLORS)
-                dataSet.valueTextSize = 14f
-                dataSet.setDrawValues(false)
+                    val dataSet = PieDataSet(pieEntries, "Total Expenses")
+                    dataSet.setColors(CHART_COLORS)
+                    dataSet.valueTextSize = 14f
+                    dataSet.setDrawValues(false)
 //                dataSet.sliceSpace = 100f
-                val data = PieData(dataSet)
-                binding.chart.apply {
-                    animateY(1000, Easing.EasingOption.EaseInOutCubic)
-                    isDrawHoleEnabled = true
-                    this.data = data
-                    centerText = NumberFormat.getCurrencyInstance()
-                        .format(list.map { it.expensePrice }.reduce { acc, expense ->
-                            acc + expense
-                        })
-                    setDrawEntryLabels(false)
-                    legend.isEnabled = false
+                    val data = PieData(dataSet)
+                    binding.chart.apply {
+                        animateY(1000, Easing.EasingOption.EaseInOutCubic)
+                        isDrawHoleEnabled = true
+                        this.data = data
+                        centerText = NumberFormat.getCurrencyInstance()
+                            .format(list.map { it.expensePrice }.reduce { acc, expense ->
+                                acc + expense
+                            })
+                        setDrawEntryLabels(false)
+                        legend.isEnabled = false
 //                    holeRadius = 35f
-                    setCenterTextSize(14f)
-                    setDescription("")
+                        setCenterTextSize(14f)
+                        setDescription("")
+                    }
+                    binding.chart.invalidate()
+                    binding.dateTextView.text = Util.getMonthAndYear(System.currentTimeMillis())
+
+                } catch (t: Throwable) {
+                    t.printStackTrace()
                 }
-//                view.chart.setExtraOffsets(5f, 5f, 10f, 5f)
-                binding.chart.invalidate()
-                binding.dateTextView.text = Util.getMonthAndYear(System.currentTimeMillis())
-
-                binding.dataListView.adapter = ListAdapter(requireContext(), list)
-                binding.dataListView.setOnItemClickListener { _, _, position, id ->
-                    AlertDialog.Builder(this@DataFragment.requireContext())
-                        .setMessage(
-                            "Description: ${list[position].description}\n Cost: ${
-                                NumberFormat.getCurrencyInstance().format(
-                                    list[position].expensePrice
-                                )
-                            }"
-                        )
-                        .setPositiveButton("Done") { p0, p1 -> }.create().show()
-
-
-                }
-        })
+            }
+        }
     }
 
 
-
-
-
     private fun composeAndSendEmail(list: List<Expense>) {
-
+        Log.d("Ares",list.toString())
         var expenseMessage = ""
         list.forEach {
             expenseMessage += getString(
@@ -178,9 +262,10 @@ class DataFragment : Fragment() {
         }
         expenseMessage += getString(
             R.string.monthly_total,
-            NumberFormat.getCurrencyInstance().format(list.map { it.expensePrice }.reduce { acc, expense ->
-                acc + expense
-            })
+            NumberFormat.getCurrencyInstance()
+                .format(list.map { it.expensePrice }.reduce { acc, expense ->
+                    acc + expense
+                })
         )
         expenseMessage += getString(R.string.thank_you)
         val intent = Intent(Intent.ACTION_SENDTO)
@@ -189,9 +274,9 @@ class DataFragment : Fragment() {
             Intent.EXTRA_SUBJECT,
             getString(R.string.email_subject, Util.getMonthAndYear(System.currentTimeMillis()))
         )
+
+        Log.d("Ares",expenseMessage)
         intent.putExtra(Intent.EXTRA_TEXT, expenseMessage)
-        if (intent.resolveActivity(requireActivity().packageManager) != null) {
-            startActivity(intent)
-        }
+        Intent.createChooser(intent,"Send Budget Details")
     }
 }
--- app/src/main/java/com/example/android/shissona/viewModels/DataViewModel.kt
+++ app/src/main/java/com/example/android/shissona/viewModels/DataViewModel.kt
@@ -4,9 +4,11 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asFlow
 import com.example.android.shissona.database.AppDatabase
 import com.example.android.shissona.database.Expense
 import com.example.android.shissona.database.ExpenseDao
+import kotlinx.coroutines.flow.Flow
 
 class DataViewModel(application: Application) : AndroidViewModel(application) {
     private val dao: ExpenseDao by lazy {
@@ -14,8 +16,8 @@ class DataViewModel(application: Application) : AndroidViewModel(application) {
     }
 
 
-    fun getAllExpensesForTheMonth(): LiveData<List<Expense>>{
-        return dao.getReactiveAll()
+    fun getAllExpensesForTheMonth(): Flow<List<Expense>> {
+        return dao.getReactiveAll().asFlow()
     }
 
 
